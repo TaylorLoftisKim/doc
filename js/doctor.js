99eb396e5deb65624955b7ed1a63c463c79a0e26
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-05-02 08:19:13 -0700

    fix doctor.js syntax

--- js/doctor.js
+++ js/doctor.js
@@ -4,7 +4,7 @@ function Doctor() {
 }
 
 Doctor.prototype.getDoctor = function(location, eachDoctor){
-$.get("https://api.betterdoctor.com/2016-03-01/doctors?location=portland&skip=0&limit=10&user_key=" + apiKey).then(function(response) {
+$.get("https://api.betterdoctor.com/2016-03-01/doctors?skip=0&limit=10&user_key=" + apiKey).then(function(response) {
   eachDoctor(response.data);
   console.log(location);
   }).fail(function(error) {
