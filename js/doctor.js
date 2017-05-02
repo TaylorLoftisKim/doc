680014075eb947fd937ce859823acaf15a5dda6e
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-05-02 14:59:33 -0700

    make further styling adjustments.

--- js/doctor.js
+++ js/doctor.js
@@ -6,6 +6,7 @@ function Doctor() {
 Doctor.prototype.getDoctor = function(illness) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ illness+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(output) {
+    // Only pulling four parameters for Each Doctor
      output.data.forEach(function(doctor){
       $('#showDoctors').append("<br><li> Name: " + doctor.profile.last_name + ", " + doctor.profile.first_name + " " + doctor.profile.title+ "<br> Bio: " + doctor.profile.bio + "</li><br>")
      });
