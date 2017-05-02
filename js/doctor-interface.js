71ff5bd8b4962d2f4a0be680ff28d6943835a0d7
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-05-02 09:27:40 -0700

    Refactored js files and html, add more styling.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,19 +1,12 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var eachDoctor = function(doctors) {
-  doctors.forEach(function(doctor){
-      $('#showDoctors').append(
-        "<table class='table table-striped'><thead><th>First Name</th><th>Last Name</th><th>Title</th><th>Picture</th></thead><tbody><td><p>" + doctor.profile.first_name + "</p></td><td><p>" + doctor.profile.last_name + "</p></td><td><p>" + doctor.profile.title + "</p></td><td><img src=" + doctor.profile.image_url + "></img></td></tbody></table><hr>"
-      );
-  });
-}
-
 $(document).ready(function() {
-  var newDoctor = new Doctor();
+  var searchedDoctors = new Doctor();
   $('#userLocation').submit(function() {
     event.preventDefault();
-    var location = $('#location').val();
-    $('#location').val("");
-    newDoctor.getDoctor(location, eachDoctor);
+    var illness = $('#illness').val();
+    $('#illness').val("");
+    $('#showDoctors').empty();
+    searchedDoctors.getDoctor(illness);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -3,10 +3,9 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.getDoctor = function(location, eachDoctor){
-$.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
-  eachDoctor(response.data);
-  console.log(location);
+Doctor.prototype.getDoctor = function(userIllness){
+$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ userIllness+'&location=45.5231%2C-122.6765%2C%205&user_location=37.773%2C-122.413%2C100&=0&limit=10&user_key=' + apiKey).then(function(response) {
+  console.log(userIllness);
   }).fail(function(error) {
     $('#showDoctors').text(error.responseJSON.message);
   });
