65ab25083eea14b3496b3029eaeda486bd349bfc
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-05-02 14:41:55 -0700

    fix doctor.js file errors.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,7 +2,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
   var searchedDoctors = new Doctor();
-  $('#findDoctor').submit(function() {
+  $('#findDoctor').click(function() {
     var illness = $('#illness').val();
     $('#illness').val("");
     $('#showDoctors').empty();
--- js/doctor.js
+++ js/doctor.js
@@ -3,14 +3,16 @@ var apiKey = require('./../.env').apiKey;
 function Doctor() {
 }
 
-Doctor.prototype.getDoctor = function(medicalIssue, displayDoctors){
-$.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=37.773%2C-122.413%2C100&=0&limit=10&user_key=' + apiKey)
-  .then(function(response) {
-    displayDoctors(response.data);
-    console.log(medicalIssue);
-  }).fail(function(error) {
-    $('#findDoctor').text(error.responseJSON.message);
-  });
+Doctor.prototype.getDoctor = function(illness) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ illness+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+  .then(function(output) {
+     output.data.forEach(function(doctor){
+      $('#showDoctors').append("<br><li> Name: " + doctor.profile.last_name + ", " + doctor.profile.first_name + " " + doctor.profile.title+ "<br> Bio: " + doctor.profile.bio + "</li><br>")
+     });
+     console.log(output);
+    })
+   .fail(function(error){
+    });
 };
 
 exports.doctorModule = Doctor;
