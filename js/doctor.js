eed1d7eb0f8631d49c33708f7df3d4558a914de2
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-05-02 23:42:29 -0700

    remove console logs from js file.

--- js/doctor.js
+++ js/doctor.js
@@ -6,14 +6,13 @@ function Doctor() {
 Doctor.prototype.getDoctor = function(illness) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ illness+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(output) {
-    // Only pulling four parameters for Each Doctor
      output.data.forEach(function(doctor){
+       // Only pulling four parameters for Each Doctor
       $('#showDoctors').append("<br><li><strong> Name:</strong> " + doctor.profile.last_name + ", " + doctor.profile.first_name + " " + doctor.profile.title + "<br><strong> Bio:</strong> " + doctor.profile.bio + "</li><br>")
-     });
-     console.log(output);
+      });
     })
-   .fail(function(error){
-    });
+    .fail(function(error){
+  });
 };
 
 exports.doctorModule = Doctor;
